86bd90c5ddfb2ba1091ae2d2283c673b408afd57
DavidLagoUTN <98719124+DavidLagoUTN@users.noreply.github.com>
2022-05-14 23:48:44 -0300

    Add files via upload

---
+++ TP_[2]/src/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "inputs.h"
+#include "ArrayPassenger.h"
+
+int main() {
+	setbuf(stdout,NULL);
+	char salir = 'n';
+	do {
+		switch (menuVuelos()) {
+		case 1:
+			printf("\n1. Alta pasajero\n\n");
+			break;
+		case 2:
+			printf("\n2. Modificar pasajero\n\n");
+			break;
+		case 3:
+			printf("\n3. Baja pasajero\n\n");
+			break;
+		case 4:
+			printf("\n4. Informar\n\n");
+			break;
+		case 0:
+			printf("\n0. Salir\n\n");
+			salir = 's';
+			break;
+		default:
+			printf("\n>>>Opcion invalida<<<\n\n");
+		}
+	} while (salir != 's');
+
+	printf("Adios!!!.");
+return 0;
+}

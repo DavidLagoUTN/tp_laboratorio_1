992baa57f6a2bc8ca267653b1538a44cd2cef293
DavidLagoUTN <98719124+DavidLagoUTN@users.noreply.github.com>
2022-05-14 23:27:59 -0300

    Add files via upload

---
+++ TP_[2]/src/ArrayPassenger.h
@@ -0,0 +1,14 @@
+#ifndef ARRAYPASSENGER_H_
+#define ARRAYPASSENGER_H_
+
+struct {
+	int id;
+	char name[51];
+	char lastName[51];
+	float price;
+	char flycode[10];
+	int typePassenger;
+	int isEmpty;
+}typedef Passenger;
+
+#endif /* ARRAYPASSENGER_H_ */
